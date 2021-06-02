558d3350c6b0dcdf66b6ba80f8a31062c5b47a36
Riyaan Bakhda <riyaanbakhda2@gmail.com>
2021-06-02 13:31:38 -0400

    Fixed submit form

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -33,11 +33,11 @@ $(function () {
                 success: function () {
                     // Success message
                     $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
+                    // $("#success > .alert-success")
+                    //     .html(
+                    //         "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                    //     )
+                    //     .append("</button>");
                     $("#success > .alert-success").append(
                         "<strong>Hey, " + firstName + "! Welcome to our Newsletter.</strong>"
                     );
@@ -66,11 +66,11 @@ $(function () {
 
                     // Success message
                     $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
+                    // $("#success > .alert-success")
+                    //     .html(
+                    //         "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                    //     )
+                    //     .append("</button>");
                     $("#success > .alert-success").append(
                         "<strong>Hey, " + firstName + "! Welcome to our Newsletter.</strong>"
                     );
